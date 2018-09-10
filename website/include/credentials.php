3ceed52c0d7bfa14824636a1e13da60d84c8d762
erikmearidy <42616954+erikmearidy@users.noreply.github.com>
2018-09-10 12:43:16 -0400

    Update credentials.php

--- website/include/credentials.php
+++ website/include/credentials.php
@@ -5,7 +5,7 @@ class Credentials {
     // Declare the credentials to the database
     public $servername = "localhost";
     public $serverusername = "root";
-    public $serverpassword = "password";
+    public $serverpassword = "hunter2";
     public $serverdb = "it5236";
 
 }
